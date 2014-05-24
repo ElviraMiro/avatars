108c422147954d6d4933db7e58e54d7b097d54e3
Elvira <karashistka@gmail.com>
2014-05-24 16:01:37 +0300

    Only append

--- client/views/home.js
+++ client/views/home.js
@@ -1,10 +1,14 @@
 Session.setDefault("sessionId", null);
 Session.setDefault("getImage", false);
 Session.setDefault("selection", null);
+Session.setDefault("lookImage", false);
 
 Template.home.helpers({
 	getImage: function() {
 		return Session.get("getImage");
+	},
+	lookImage: function() {
+		return Session.get("lookImage");
 	}
 });
 
@@ -18,7 +22,9 @@ Template.addUserPhoto.events({
 	'click .frames': function() {
 		$("input.frames").each(function() {
 			if (this.checked) {
-				Session.set("selectedFrame", Frames.findOne(this.id));
+				var frame = Frames.findOne(this.id);
+				Session.set("selectedFrame", frame);
+				Session.set("frameUrl", frame.url());
 			}
 		})
 	},
@@ -74,6 +80,34 @@ Template.editUserPhoto.events({
   			var writeStream = image.createWriteStream('images');
 			gm(readStream).crop(selection.width, selection.height, selection.x1, selection.y1).resize(630, 475).stream().pipe(writeStream);*/
 			Meteor.call('addFrame', image.copies.images.key, Session.get("selectedFrame").copies.frameimages.key, Session.get("selection"), Session.get("selectedFrame").metadata.selection);
+			Session.set("getImage", false);
+			Session.set("lookImage", true);
 		}
+	}
+});
+
+Template.lookImageData.helpers({
+	imageUrl: function() {
+		var image = Images.findOne({"metadata.sessionId": Session.get("sessionId")});
+		if (image) {
+			return image.url();
+		}
+	},
+	frameUrl: function() {
+		return Session.get("frameUrl");
+	},
+	imageStyle: function() {
+		var frame = Session.get("selectedFrame"),
+			result = "";
+		if (frame) {
+			bottom = 15+parseInt(frame.metadata.selection.y1);
+			left = 15+parseInt(frame.metadata.selection.x1);
+			result = "z-index: 1; position: absolute; bottom: "+bottom+"px; left: "+left+"px;";
+		}
+		return result;
+	},
+	frameStyle: function() {
+		var result = "z-index: 2; position: absolute; bottom: 15px; left: 15px;";
+		return result;
 	}
 });
--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ Meteor.methods({
 			imageMagick(imagename).crop(selection.width, selection.height, selection.x1, selection.y1).resize(parseInt(selFrame.width), parseInt(selFrame.height)).write(imagename, function (err) {
 				if (!err) console.log('done1');
 			});
-			var point = "+".concat(selFrame.x1,"+", selFrame.y1);
-			var drawcom = "image Over ".concat(selFrame.x1, ",", selFrame.y1, " ", selFrame.width, ",", selFrame.height, " ", imagename);
-			imageMagick(framename).draw(drawcom).write(imagename, function (err) {
+			//var point = "+".concat(selFrame.x1,"+", selFrame.y1);
+			//var drawcom = "image Over ".concat(selFrame.x1, ",", selFrame.y1, " ", selFrame.width, ",", selFrame.height, " ", imagename);
+			/*imageMagick().in("-page",point).in(imagename).in("-page", "+0+0").in(framename).write(imagename, function (err) {
 				console.log(err);
 				if (!err) console.log('done2');
-			});
+			});*/
 		}
 	}
 })
